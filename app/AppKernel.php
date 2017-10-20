d2f6fa00c78367b58bb8f8699a3cb3bc821d7a7e
torkhani <torkhani.moetez@gmail.com>
2017-10-20 16:17:42 +0200

    config media sonata bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new AdminBundle\AdminBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
